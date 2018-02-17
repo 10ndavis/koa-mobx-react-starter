eba719ac26e62ead6a9c5bf884ed78d1128bcff1
Nathan Brewer-Davis <10ndavis10@gmail.com>
2018-02-17 12:48:29 -0600

    git init
    git init

--- views/base.marko.js
+++ views/base.marko.js
@@ -1,24 +1,33 @@
-// Compiled using marko@4.2.8 - DO NOT EDIT
+// Compiled using marko@4.7.5 - DO NOT EDIT
 "use strict";
 
-var marko_template = module.exports = require("marko/html").t(__filename),
-    marko_helpers = require("marko/runtime/html/helpers"),
+var marko_template = module.exports = require("marko/src/html").t(__filename),
+    marko_componentType = "/koa-mobx-react-starter$1.0.0/views/base.marko",
+    components_helpers = require("marko/src/components/helpers"),
+    marko_renderer = components_helpers.r,
+    marko_defineComponent = components_helpers.c,
+    marko_helpers = require("marko/src/runtime/html/helpers"),
     marko_escapeXml = marko_helpers.x,
     marko_escapeXmlAttr = marko_helpers.xa,
+    marko_loadTag = marko_helpers.t,
+    component_globals_tag = marko_loadTag(require("marko/src/components/taglib/component-globals-tag")),
     marko_str = marko_helpers.s,
     marko_escapeScript = marko_helpers.xs,
-    marko_loadTag = marko_helpers.t,
-    init_components_tag = marko_loadTag(require("marko/components/taglib/init-components-tag")),
-    await_reorderer_tag = marko_loadTag(require("marko/taglibs/async/await-reorderer-tag"));
+    init_components_tag = marko_loadTag(require("marko/src/components/taglib/init-components-tag")),
+    await_reorderer_tag = marko_loadTag(require("marko/src/taglibs/async/await-reorderer-tag"));
 
-function render(input, out) {
+function render(input, out, __component, component, state) {
   var data = input;
 
   out.w("<!DOCTYPE html><html lang=\"en\"><head><title>" +
     marko_escapeXml(data.title) +
     "</title><meta charset=\"utf-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0\"><meta name=\"description\" content=\"" +
     marko_escapeXmlAttr(data.description) +
-    "\"><link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"/apple-touch-icon.png\"><link rel=\"icon\" type=\"image/png\" href=\"/favicon-32x32.png\" sizes=\"32x32\"><link rel=\"icon\" type=\"image/png\" href=\"/favicon-16x16.png\" sizes=\"16x16\"><link rel=\"manifest\" href=\"/manifest.json\"><meta name=\"application-name\" content=\"Vibescout\"><link rel=\"stylesheet\" type=\"text/css\" href=\"/dist/styles.css\"></head><body><div id=\"root\">" +
+    "\"><link rel=\"apple-touch-icon\" sizes=\"180x180\" href=\"/apple-touch-icon.png\"><link rel=\"icon\" type=\"image/png\" href=\"/favicon-32x32.png\" sizes=\"32x32\"><link rel=\"icon\" type=\"image/png\" href=\"/favicon-16x16.png\" sizes=\"16x16\"><link rel=\"manifest\" href=\"/manifest.json\"><meta name=\"application-name\" content=\"Vibescout\"><link rel=\"stylesheet\" type=\"text/css\" href=\"/dist/styles.css\"></head><body>");
+
+  component_globals_tag({}, out);
+
+  out.w("<div id=\"root\">" +
     marko_str(data.html) +
     "</div><script type=\"text/javascript\">window.__INITIAL_STATE__ = JSON.stringify(" +
     marko_escapeScript(data.initialState) +
@@ -26,16 +35,23 @@ function render(input, out) {
 
   init_components_tag({}, out);
 
-  await_reorderer_tag({}, out);
+  await_reorderer_tag({}, out, __component, "16");
 
   out.w("</body></html>");
 }
 
-marko_template._ = render;
+marko_template._ = marko_renderer(render, {
+    ___implicit: true,
+    ___type: marko_componentType
+  });
+
+marko_template.Component = marko_defineComponent({}, marko_template._);
 
 marko_template.meta = {
+    id: "/koa-mobx-react-starter$1.0.0/views/base.marko",
     tags: [
-      "marko/components/taglib/init-components-tag",
-      "marko/taglibs/async/await-reorderer-tag"
+      "marko/src/components/taglib/component-globals-tag",
+      "marko/src/components/taglib/init-components-tag",
+      "marko/src/taglibs/async/await-reorderer-tag"
     ]
   };
