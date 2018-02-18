5d0155cf494b3ef70a3db8dd20441d94fb86ec34
Nathan Brewer-Davis <10ndavis10@gmail.com>
2018-02-18 17:02:45 -0600

    created new rem function for sass
    created new rem function for sass

--- src/client/react/SignIn.js
+++ src/client/react/SignIn.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { observer, inject } from 'mobx-react';
+import FloatingLabel from 'floating-label-react'
+
 
 @inject("CounterStore") @observer
 class SignIn extends Component {
@@ -8,8 +10,25 @@ class SignIn extends Component {
 
   render() {
     return (
-      <div>
+      <div className='SignIn'>
         Sign In page
+
+        <div className='SignIn__box'>
+          <FloatingLabel
+            id='username'
+            name='Username'
+            placeholder='Email'
+            type='text'
+          />
+
+          <FloatingLabel
+            id='password'
+            name='Password'
+            placeholder='Password'
+            type='password'
+          />
+          <button>Sign In</button>
+        </div>
       </div>
     );
   }
